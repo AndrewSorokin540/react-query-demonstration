8099850e9b712600404886687880a69623487aaa
Андрей <andrewsorokinwork@gmail.com>
2022-03-18 22:58:19 +0700

    queryClient default options for queries

--- src/react-query-hooks/queryClient.ts
+++ src/react-query-hooks/queryClient.ts
@@ -1,3 +1,11 @@
 import { QueryClient } from "react-query";
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retryDelay: (attemptIndex) => Math.min(1000 * 2 ** attemptIndex, 30000),
+      retry: 2,
+    },
+  },
+});
